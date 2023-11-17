2ba6f934f2d9717f706996324149650878aaa32f
me <d-boldin@list.ru>
2023-11-17 11:51:03 +0300

    d

--- ExtraTasks/sr#1,2/paradox/__main__.py
+++ ExtraTasks/sr#1,2/paradox/__main__.py
@@ -1,5 +1,10 @@
+# -*- coding: cp1251 -*-
 from dddd import N
 from birthday import birth
-print(N(100), '%')
-print(birth(100,10))
+print('Выберите парадокс : \n 1.Монти Холла \n 2.Дней рождений')
+pick = int(input())
+if pick == 1:
+    print(N(int(input('Введите кол-во игр: '))), '%')
+else:
+    print(birth(int(input('Введите кол-во людей: ')),int(input('Введите кол-во попыток: '))))
 
