ab1d5eea47a8a54c76a8a3ab3a04a7b345301d49
FuNNyN <d-boldin@list.ru>
2024-01-09 11:19:40 +0300

    .

---
+++ ExtraTasks/prac_7/viewier.py
@@ -0,0 +1,18 @@
+from ExtraTasks.prac_7.main import *
+
+
+def show():
+
+    print("Чтобы выйти введите \"0\".")
+
+    while True:
+        input_user = input("Введите название книге: ")
+        if input_user == '0':
+            break
+        data = get_books(input_user)
+        print(data)
+        print(get_totals(data))
+
+
+if __name__ == "__main__":
+    show()
